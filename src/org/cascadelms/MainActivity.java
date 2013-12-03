2fdb0a4411b2e15042d305492be24ba3df6edefd
Cosmic Chip Socket <hagersdk@mail.uc.edu>
2013-12-02 20:49:36 -0500

    Add (empty) navigation drawer

--- src/org/cascadelms/MainActivity.java
+++ src/org/cascadelms/MainActivity.java
@@ -1,11 +1,18 @@
 package org.cascadelms;
 
+import org.cascadelms.fragments.SocialStreamFragment;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
 /**
@@ -15,6 +22,10 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity
 {
     private static final String PREFS_AUTH = "AuthenticationData";
+    
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,11 +37,38 @@ public class MainActivity extends ActionBarActivity
         boolean loggedIn = preferences.getBoolean("loggedIn", false);
 
         if (loggedIn)
-            setContentView(R.layout.activity_main);
-        else
         {
-            openLoginActivity();
+            setContentView(R.layout.activity_main);
+            
+            mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+            mDrawerList = (ListView)findViewById(R.id.left_drawer);
+            
+            // Set up drawer toggle
+            mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                    R.drawable.ic_drawer, R.string.drawer_open,
+                    R.string.drawer_close);
+            
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+            
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            
+            // Jump to default fragment
+            SocialStreamFragment fragment = new SocialStreamFragment();
+            
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.content_frame, fragment)
+                                      .commit();
         }
+        else
+            openLoginActivity();
+    }
+    
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 
     @Override
@@ -40,10 +78,20 @@ public class MainActivity extends ActionBarActivity
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        if (mDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        
         switch (item.getItemId())
         {
         case R.id.action_logout:
@@ -76,5 +124,10 @@ public class MainActivity extends ActionBarActivity
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(intent);
         finish();
+    }
+    
+    private void registerCourse(int id, String courseName)
+    {
+        
     }
 }
