83af317127d6fdbaa009d0804fb09ef4e6c40ca2
Alex Morgan <axemorgan@gmail.com>
2014-03-12 19:49:48 -0400

    Added static newInstance method to CourseBlogFragment.

--- src/org/cascadelms/fragments/AssignmentsFragment.java
+++ src/org/cascadelms/fragments/AssignmentsFragment.java
@@ -17,13 +17,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AssignmentsFragment extends ListFragment implements LoaderCallbacks<List<Assignment>>
+public class AssignmentsFragment extends ListFragment implements
+		LoaderCallbacks<List<Assignment>>
 {
 	private AssignmentsDataSource assigmentsDataSource;
-	
+
 	/* Constants */
 	private static final String ARGS_COURSE = "org.cascade.args_course";
-	
+
 	public static AssignmentsFragment newInstance( Course course )
 	{
 		Bundle args = new Bundle();
@@ -32,81 +33,90 @@ public class AssignmentsFragment extends ListFragment implements LoaderCallbacks
 		fragment.setArguments( args );
 		return fragment;
 	}
-	
+
 	@Override
 	public void onCreate( Bundle savedInstanceState )
 	{
 		/* Creates the data source and begins loading. */
 		this.assigmentsDataSource = FakeDataSource.getInstance();
-		this.getActivity().getSupportLoaderManager().initLoader( LoaderCodes.LOADER_CODE_ASSIGNMENTS, null, this ).forceLoad();
-		
-		super.onCreate(savedInstanceState);
+		this.getActivity().getSupportLoaderManager()
+				.initLoader( LoaderCodes.LOADER_CODE_ASSIGNMENTS, null, this )
+				.forceLoad();
+
+		super.onCreate( savedInstanceState );
 	}
-	
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState)
-    {
-        super.onCreateView( inflater, container, savedInstanceState );     
-        View view = inflater.inflate( R.layout.fragment_assignments, container, false );       
-        return view;
-    }
-    
-    /**
-     * Retrieves the {@link Course} provided as an argument to this Fragment.  The value cannot be stored, 
-     * because the Fragment may be recreated by the FragmentManager at any time.  
-     * @return the <code>Course</code>
-     */
-    private Course getCourse()
-    {
-    	return this.getArguments().getParcelable( ARGS_COURSE );
-    }
 
 	@Override
-	public Loader<List<Assignment>> onCreateLoader( int id, Bundle args ) 
+	public View onCreateView( LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState )
+	{
+		super.onCreateView( inflater, container, savedInstanceState );
+		View view = inflater.inflate( R.layout.fragment_assignments, container,
+				false );
+		return view;
+	}
+
+	/**
+	 * Retrieves the {@link Course} provided as an argument to this Fragment.
+	 * The value cannot be stored, because the Fragment may be recreated by the
+	 * FragmentManager at any time.
+	 * 
+	 * @return the <code>Course</code>
+	 */
+	private Course getCourse()
+	{
+		return this.getArguments().getParcelable( ARGS_COURSE );
+	}
+
+	@Override
+	public Loader<List<Assignment>> onCreateLoader( int id, Bundle args )
 	{
 		switch( id )
 		{
-		case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
-		{
-			return new AssignmentsLoader( this.getActivity(), assigmentsDataSource, this.getCourse().getId() );
-		}
-		default:
-		{
-			return null;
-		}
+			case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
+			{
+				return new AssignmentsLoader( this.getActivity(),
+						assigmentsDataSource, this.getCourse().getId() );
+			}
+			default:
+			{
+				return null;
+			}
 		}
 	}
 
 	@Override
 	public void onLoadFinished( Loader<List<Assignment>> loader,
-			List<Assignment> data ) 
+			List<Assignment> data )
 	{
 		switch( loader.getId() )
 		{
-		case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
-		{
-			/* TODO Use this data in the Fragment. */
-			break;
+			case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
+			{
+				/* TODO Use this data in the Fragment. */
+				break;
+			}
 		}
-		}
-		
+
 	}
 
 	@Override
-	public void onLoaderReset( Loader<List<Assignment>> loader ) 
+	public void onLoaderReset( Loader<List<Assignment>> loader )
 	{
 		switch( loader.getId() )
 		{
-		case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
-		{
-			/* TODO Anything that uses data from the Loader needs to clear its data here. */
-			break;
-		}
+			case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
+			{
+				/*
+				 * TODO Anything that uses data from the Loader needs to clear
+				 * its data here.
+				 */
+				break;
+			}
 		}
 	}
-	
-	public interface AssignmentsDataSource 
+
+	public interface AssignmentsDataSource
 	{
 		public List<Assignment> getAssignmentsForCourse( int courseId );
 	}
