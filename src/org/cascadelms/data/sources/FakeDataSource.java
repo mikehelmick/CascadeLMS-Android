4fc0b3d8c320eba6e541c97e089804fa5fcc9e13
Alex Morgan <axemorgan@gmail.com>
2014-03-13 19:35:03 -0400

    Added some directories to the test data.

--- src/org/cascadelms/data/sources/FakeDataSource.java
+++ src/org/cascadelms/data/sources/FakeDataSource.java
@@ -19,147 +19,15 @@ import org.cascadelms.fragments.SocialStreamFragment.StreamDataSource;
 public class FakeDataSource implements CourseDataSource, DocumentsDataSource,
 		AssignmentsDataSource, BlogDataSource, StreamDataSource
 {
-	private static ArrayList<Course> courseList;
-
-	private static ArrayList<Document> documentsListDesign;
-	private static ArrayList<Document> documentsListMachine;
-	private static ArrayList<Document> documentsListAI;
-	private static ArrayList<Document> documentsListInfo;
-	private static ArrayList<Document> documentsListUI;
-
 	/* TODO Lists with no fake data in them yet. */
 	private static ArrayList<Assignment> emptyAssignmentsList;
 	private static ArrayList<BlogPost> emptyBlogPostList;
 	private static ArrayList<StreamItem> emptyStreamItemList;
 
-	/* Constants */
-	private static final int COURSE_ID_DESIGN = 0;
-	private static final int COURSE_ID_MACHINE_LEARNING = 1;
-	private static final int COURSE_ID_AI = 2;
-	private static final int COURSE_ID_INFO_RETRIEVAL = 3;
-	private static final int COURSE_ID_UI = 4;
-
 	/* Static initializer */
 	{
-		/* Builds the course list */
-		courseList = new ArrayList<Course>();
-		courseList.add( ( new Course.Builder( COURSE_ID_DESIGN,
-				"CS5002-Senior Design" ) ).build() );
-		courseList.add( ( new Course.Builder( COURSE_ID_MACHINE_LEARNING,
-				"CS6037-Machine Learning" ) ).build() );
-		courseList.add( ( new Course.Builder( COURSE_ID_AI,
-				"CS6033-Artificial Intellegence" ) ).build() );
-		courseList.add( ( new Course.Builder( COURSE_ID_INFO_RETRIEVAL,
-				"CS6054-Info Retrieval" ) ).build() );
-		courseList.add( ( new Course.Builder( COURSE_ID_UI,
-				"CS6067-User Interface" ) ).build() );
-
-		/* Builds the documents list for each course. */
-		documentsListDesign = new ArrayList<Document>();
-		try
-		{
-			documentsListDesign.add( Document.Builder.getFileBuilder( 0,
-					"Syllabus-2013", ".pdf", 20000,
-					new URL( "http://example.com/Syllabus2013.pdf" ) ).build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 1,
-					"Project Teams", ".txt", 5000,
-					new URL( "http://example.com/ProjectTeams.txt" ) ).build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 2,
-					"Sample Project Poster", ".png", 200000,
-					new URL( "http://example.com/Sample_project_poster.png" ) )
-					.build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 3,
-					"Abstract Edits", ".docx", 25000,
-					new URL( "http://example.com/Abstract_edits.docx" ) )
-					.build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 4,
-					"PreviousProjects1", ".pdf", 25000,
-					new URL( "http://example.com/Previous_Projects1.pdf" ) )
-					.build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 5,
-					"PreviousProjects2", ".pdf", 25000,
-					new URL( "http://example.com/Previous_Projects2.pdf" ) )
-					.build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 6,
-					"PreviousProjects3", ".pdf", 25000,
-					new URL( "http://example.com/Previous_Projects3.pdf" ) )
-					.build() );
-			documentsListDesign.add( Document.Builder.getFileBuilder( 7,
-					"PreviousProjects4", ".pdf", 25000,
-					new URL( "http://example.com/Previous_Projects4.pdf" ) )
-					.build() );
-		} catch( MalformedURLException e )
-		{
-		}
-		documentsListMachine = new ArrayList<Document>();
-		try
-		{
-			documentsListMachine.add( Document.Builder.getFileBuilder( 10,
-					"MachineSyllabus-2013", ".pdf", 20000,
-					new URL( "http://example.com/Syllabus2013.pdf" ) ).build() );
-			documentsListMachine.add( Document.Builder.getFileBuilder( 11,
-					"Supervised Learning", ".ppt", 50000,
-					new URL( "http://example.com/Supervised.ppt" ) ).build() );
-			documentsListMachine.add( Document.Builder.getFileBuilder( 12,
-					"Transduction Training Cases", ".dat", 543210,
-					new URL( "http://example.com/training-cases.dat" ) )
-					.build() );
-			documentsListMachine.add( Document.Builder.getFileBuilder( 13,
-					"Cluster Analysis", ".ppt", 25000,
-					new URL( "http://example.com/Clustering.ppt" ) ).build() );
-		} catch( MalformedURLException e )
-		{
-		}
-		documentsListAI = new ArrayList<Document>();
-		try
-		{
-			documentsListAI.add( Document.Builder.getFileBuilder( 100,
-					"AI_Syllabus_2013-14", ".docx", 20000,
-					new URL( "http://example.com/AI_Syllabus_2013-14.docx" ) )
-					.build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 101,
-					"Lecture_1", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 102,
-					"Lecture_2", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 103,
-					"Lecture_3", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 104,
-					"Lecture_4", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 105,
-					"Lecture_5", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 106,
-					"Lecture_6", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 107,
-					"Lecture_7", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-			documentsListAI.add( Document.Builder.getFileBuilder( 108,
-					"Lecture_8", ".ppt", 50000,
-					new URL( "http://example.com/Lecture.ppt" ) ).build() );
-		} catch( MalformedURLException e )
-		{
-		}
-		documentsListInfo = new ArrayList<Document>();
-		try
-		{
-			documentsListInfo.add( Document.Builder.getFileBuilder( 10,
-					"Info_retrieval2013", ".pdf", 20000,
-					new URL( "http://example.com/Syllabus2013.pdf" ) ).build() );
-			documentsListMachine.add( Document.Builder.getFileBuilder( 11,
-					"Lecture Notes on a very obscure topic with a long title",
-					".ppt", 50000,
-					new URL( "http://example.com/Supervised.ppt" ) ).build() );
-		} catch( MalformedURLException e )
-		{
-		}
-
-		/* Leaves the UI course as an empty list with no Documents. */
-		documentsListUI = new ArrayList<Document>();
+		FakeDataSource.initCourseData();
+		FakeDataSource.initDocumentsData();
 
 		/* Builds Assignment lists for each course. */
 		emptyAssignmentsList = new ArrayList<Assignment>();
@@ -189,13 +57,164 @@ public class FakeDataSource implements CourseDataSource, DocumentsDataSource,
 	}
 
 	/* CourseDataSource */
+	/* Course ID Constants */
+	private static final int COURSE_ID_DESIGN = 0;
+	private static final int COURSE_ID_MACHINE_LEARNING = 1;
+	private static final int COURSE_ID_AI = 2;
+	private static final int COURSE_ID_INFO_RETRIEVAL = 3;
+	private static final int COURSE_ID_UI = 4;
+
+	private static ArrayList<Course> courseList;
+
+	private static void initCourseData()
+	{
+		/* Builds the course list */
+		courseList = new ArrayList<Course>();
+		courseList.add( ( new Course.Builder( COURSE_ID_DESIGN,
+				"CS5002-Senior Design" ) ).build() );
+		courseList.add( ( new Course.Builder( COURSE_ID_MACHINE_LEARNING,
+				"CS6037-Machine Learning" ) ).build() );
+		courseList.add( ( new Course.Builder( COURSE_ID_AI,
+				"CS6033-Artificial Intellegence" ) ).build() );
+		courseList.add( ( new Course.Builder( COURSE_ID_INFO_RETRIEVAL,
+				"CS6054-Info Retrieval" ) ).build() );
+		courseList.add( ( new Course.Builder( COURSE_ID_UI,
+				"CS6067-User Interface" ) ).build() );
+	}
+
 	@Override
 	public List<Course> getAvailableCourses()
 	{
 		return courseList;
 	}
 
-	/* DocumentsDataSource Methods */
+	/* DocumentsDataSource */
+	/* ID Constants for Directories */
+	private static final int DOCUMENT_FOLDER_LECTURE_NOTES = 555;
+	private static final int DOCUMENT_FOLDER_PAST_PROJECTS = 444;
+
+	private static ArrayList<Document> documentsListDesign;
+	private static ArrayList<Document> documentsListMachine;
+	private static ArrayList<Document> documentsListAI;
+	private static ArrayList<Document> documentsListInfo;
+	private static ArrayList<Document> documentsListUI;
+	private static ArrayList<Document> directoryListPreviousProjects;
+	private static ArrayList<Document> directoryListLectures;
+
+	private static void initDocumentsData()
+	{
+		/* Builds Documents lists for each course. */
+		documentsListDesign = new ArrayList<Document>();
+		try
+		{
+			/* List for Senior Design */
+			documentsListDesign.add( Document.Builder.getFileBuilder( 0,
+					"Syllabus-2013", ".pdf", 20000,
+					new URL( "http://example.com/Syllabus2013.pdf" ) ).build() );
+			documentsListDesign.add( Document.Builder.getFileBuilder( 1,
+					"Project Teams", ".txt", 5000,
+					new URL( "http://example.com/ProjectTeams.txt" ) ).build() );
+			documentsListDesign.add( Document.Builder.getFileBuilder( 2,
+					"Sample Project Poster", ".png", 200000,
+					new URL( "http://example.com/Sample_project_poster.png" ) )
+					.build() );
+			documentsListDesign.add( Document.Builder.getFileBuilder( 3,
+					"Abstract Edits", ".docx", 25000,
+					new URL( "http://example.com/Abstract_edits.docx" ) )
+					.build() );
+			documentsListDesign.add( Document.Builder.getFolderBuilder(
+					DOCUMENT_FOLDER_PAST_PROJECTS, "Previous Projects" )
+					.build() );
+
+			/* A list for the Previous Projects subfolder in Senior Design. */
+			directoryListPreviousProjects = new ArrayList<Document>();
+			directoryListPreviousProjects.add( Document.Builder.getFileBuilder(
+					4, "PreviousProjects1", ".pdf", 25000,
+					new URL( "http://example.com/Previous_Projects1.pdf" ) )
+					.build() );
+			directoryListPreviousProjects.add( Document.Builder.getFileBuilder(
+					5, "PreviousProjects2", ".pdf", 25000,
+					new URL( "http://example.com/Previous_Projects2.pdf" ) )
+					.build() );
+			directoryListPreviousProjects.add( Document.Builder.getFileBuilder(
+					6, "PreviousProjects3", ".pdf", 25000,
+					new URL( "http://example.com/Previous_Projects3.pdf" ) )
+					.build() );
+			directoryListPreviousProjects.add( Document.Builder.getFileBuilder(
+					7, "PreviousProjects4", ".pdf", 25000,
+					new URL( "http://example.com/Previous_Projects4.pdf" ) )
+					.build() );
+
+			/* List for Machine Learning */
+			documentsListMachine = new ArrayList<Document>();
+			documentsListMachine.add( Document.Builder.getFileBuilder( 10,
+					"MachineSyllabus-2013", ".pdf", 20000,
+					new URL( "http://example.com/Syllabus2013.pdf" ) ).build() );
+			documentsListMachine.add( Document.Builder.getFileBuilder( 11,
+					"Supervised Learning", ".ppt", 50000,
+					new URL( "http://example.com/Supervised.ppt" ) ).build() );
+			documentsListMachine.add( Document.Builder.getFileBuilder( 12,
+					"Transduction Training Cases", ".dat", 543210,
+					new URL( "http://example.com/training-cases.dat" ) )
+					.build() );
+			documentsListMachine.add( Document.Builder.getFileBuilder( 13,
+					"Cluster Analysis", ".ppt", 25000,
+					new URL( "http://example.com/Clustering.ppt" ) ).build() );
+
+			/* List for Artificial Intelligence */
+			documentsListAI = new ArrayList<Document>();
+			documentsListAI.add( Document.Builder.getFileBuilder( 100,
+					"AI_Syllabus_2013-14", ".docx", 20000,
+					new URL( "http://example.com/AI_Syllabus_2013-14.docx" ) )
+					.build() );
+			documentsListAI.add( Document.Builder.getFolderBuilder(
+					DOCUMENT_FOLDER_LECTURE_NOTES, "Lectures" ).build() );
+
+			/* List for the Lectures sub-folder in AI */
+			directoryListLectures = new ArrayList<Document>();
+			directoryListLectures.add( Document.Builder.getFileBuilder( 101,
+					"Lecture_1", ".ppt", 50000,
+					new URL( "http://example.com/Lecture1.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 102,
+					"Lecture_2", ".ppt", 50000,
+					new URL( "http://example.com/Lecture2.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 103,
+					"Lecture_3", ".ppt", 50000,
+					new URL( "http://example.com/Lecture3.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 104,
+					"Lecture_4", ".ppt", 50000,
+					new URL( "http://example.com/Lecture4.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 105,
+					"Lecture_5", ".ppt", 50000,
+					new URL( "http://example.com/Lecture5.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 106,
+					"Lecture_6", ".ppt", 50000,
+					new URL( "http://example.com/Lecture6.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 107,
+					"Lecture_7", ".ppt", 50000,
+					new URL( "http://example.com/Lecture7.ppt" ) ).build() );
+			directoryListLectures.add( Document.Builder.getFileBuilder( 108,
+					"Lecture_8", ".ppt", 50000,
+					new URL( "http://example.com/Lecture8.ppt" ) ).build() );
+
+			/* List for Information Retrieval */
+			documentsListInfo = new ArrayList<Document>();
+			documentsListInfo.add( Document.Builder.getFileBuilder( 10,
+					"Info_retrieval2013", ".pdf", 20000,
+					new URL( "http://example.com/Syllabus2013.pdf" ) ).build() );
+			documentsListMachine.add( Document.Builder.getFileBuilder( 11,
+					"Lecture Notes on a very obscure topic with a long title",
+					".ppt", 50000,
+					new URL( "http://example.com/Supervised.ppt" ) ).build() );
+
+			/* Leaves the UI course as an empty list with no Documents. */
+			documentsListUI = new ArrayList<Document>();
+		} catch( MalformedURLException e )
+		{
+
+		}
+	}
+
 	@Override
 	public List<Document> getDocumentsForCourse( int courseId )
 	{
@@ -231,8 +250,21 @@ public class FakeDataSource implements CourseDataSource, DocumentsDataSource,
 	@Override
 	public List<Document> getDocumentsInFolder( Document folder )
 	{
-		// TODO Create some test data to return.
-		return new ArrayList<Document>();
+		switch( folder.getId() )
+		{
+			case DOCUMENT_FOLDER_LECTURE_NOTES:
+			{
+				return directoryListLectures;
+			}
+			case DOCUMENT_FOLDER_PAST_PROJECTS:
+			{
+				return directoryListPreviousProjects;
+			}
+			default:
+			{
+				return null;
+			}
+		}
 	}
 
 	/* AssignmentsDataSource Methods */
