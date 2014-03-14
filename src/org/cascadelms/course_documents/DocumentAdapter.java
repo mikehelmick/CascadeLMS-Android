3350f4a4022e5aff82594efc29e51b866c2f3a51
Alex Morgan <axemorgan@gmail.com>
2014-03-13 20:31:24 -0400

    Added a generic file icon to Documents in the DocumentsFragment list view.

--- src/org/cascadelms/course_documents/DocumentAdapter.java
+++ src/org/cascadelms/course_documents/DocumentAdapter.java
@@ -1,15 +1,54 @@
 package org.cascadelms.course_documents;
 
+import java.util.logging.Logger;
+
+import org.cascadelms.R;
 import org.cascadelms.data.models.Document;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class DocumentAdapter extends ArrayAdapter<Document> 
+public class DocumentAdapter extends ArrayAdapter<Document>
 {
 	public DocumentAdapter( Context context, int resource )
 	{
 		super( context, resource );
 	}
-	
+
+	@Override
+	public View getView( int position, View convertView, ViewGroup parent )
+	{
+		/* Re-uses the convertView, if possible. */
+		TextView textView;
+		if( convertView == null )
+		{
+			textView = (TextView) LayoutInflater.from( this.getContext() )
+					.inflate( R.layout.list_item_document, parent, false );
+		} else
+		{
+			textView = (TextView) convertView;
+		}
+		Document document = this.getItem( position );
+		textView.setText( document.getTitle() );
+		textView.setCompoundDrawablesWithIntrinsicBounds(
+				this.getIconIdForExtension( document.getFileExtension() ), 0,
+				0, 0 );
+		return textView;
+	}
+
+	private int getIconIdForExtension( String extension )
+	{
+		/* TODO Attempt to match the string to a list of extensions. */
+
+		// Drawable draw = resources.getDrawable( R.drawable.fileicon_bg );
+		// LOGGER.warning( "Drawable was " + draw.isVisible() );
+
+		return R.drawable.fileicon_bg;
+	}
+
+	Logger LOGGER = Logger.getLogger( DocumentAdapter.class.getName() );
 }
