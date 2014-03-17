20b9123260689bec45739fb92a2c2738bb82b31f
Alex Morgan <axemorgan@gmail.com>
2014-03-17 17:03:14 -0400

    Added points and status to Assignments list items
    -Assignment list items display points earned/points possible
    -Assignment list items display their status (i.e. upcoming, current,
    past due, closed)
    -Centered the list view's empty text.

--- src/org/cascadelms/fragments/AssignmentsAdapter.java
+++ src/org/cascadelms/fragments/AssignmentsAdapter.java
@@ -1,5 +1,7 @@
 package org.cascadelms.fragments;
 
+import java.util.logging.Logger;
+
 import org.cascadelms.R;
 import org.cascadelms.data.models.Assignment;
 
@@ -35,6 +37,54 @@ public class AssignmentsAdapter extends ArrayAdapter<Assignment>
 		/* Sets the list item text according to the Document's title. */
 		( (TextView) view.findViewById( R.id.list_item_assignment_title ) )
 				.setText( assignment.toString() );
+
+		/* Sets the points earned/possible text. */
+		String pointsString;
+		if( Double.compare( assignment.getPointsEarned(), Double.NaN ) == 0 )
+		{
+			pointsString = this.getContext().getString(
+					R.string.list_item_assignment_no_points,
+					assignment.getPointsPossible() );
+		} else
+		{
+			Logger.getLogger( AssignmentsAdapter.class.getName() ).info(
+					"Earned: " + assignment.getPointsEarned() );
+			pointsString = this.getContext().getString(
+					R.string.list_item_assignment_points,
+					assignment.getPointsEarned(),
+					assignment.getPointsPossible() );
+		}
+		( (TextView) view.findViewById( R.id.list_item_assignment_points ) )
+				.setText( pointsString );
+
+		/* Sets the date field to the next upcoming date of the assignment. */
+		String dateString = "";
+		if( assignment.isUpcoming() )
+		{
+			dateString = this.getContext().getString(
+					R.string.list_item_assignment_upcoming_date,
+					assignment.getOpenDate() );
+		} else if( assignment.isCurrent() )
+		{
+			dateString = this.getContext().getString(
+					R.string.list_item_assignment_due_date,
+					assignment.getDueDate() );
+		} else if( assignment.isPastDue() )
+		{
+			dateString = this.getContext().getString(
+					R.string.list_item_assignment_close_date,
+					assignment.getCloseDate() );
+		} else if( assignment.isClosed() )
+		{
+			dateString = this.getContext().getString(
+					R.string.list_item_assignment_closed );
+		} else
+		{
+			throw new IllegalStateException(
+					"The assignment must be one of upcoming/current/past/closed" );
+		}
+		( (TextView) view.findViewById( R.id.list_item_assignment_date ) )
+				.setText( dateString );
 
 		return view;
 	}
