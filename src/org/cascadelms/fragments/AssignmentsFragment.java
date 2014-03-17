085fd82f5fc827fc201d2aa3bcd886ce7a185cc9
Alex Morgan <axemorgan@gmail.com>
2014-03-17 19:19:09 -0400

    Fixed R.id collision between empty views for Documents and Assignments
    -Changed empty views to use custom IDs instead of android.R.id.empty
    -Added more Assignments test data

--- src/org/cascadelms/fragments/AssignmentsFragment.java
+++ src/org/cascadelms/fragments/AssignmentsFragment.java
@@ -16,6 +16,7 @@ import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class AssignmentsFragment extends ListFragment implements
@@ -23,6 +24,7 @@ public class AssignmentsFragment extends ListFragment implements
 {
 	private AssignmentsDataSource assigmentsDataSource;
 	private AssignmentsAdapter adapter;
+	private TextView emptyView;
 
 	/* Constants */
 	private static final String ARGS_COURSE = "org.cascade.args_course";
@@ -41,10 +43,6 @@ public class AssignmentsFragment extends ListFragment implements
 	{
 		/* Creates the data source and begins loading. */
 		this.assigmentsDataSource = FakeDataSource.getInstance();
-		this.getActivity().getSupportLoaderManager()
-				.initLoader( LoaderCodes.LOADER_CODE_ASSIGNMENTS, null, this )
-				.forceLoad();
-
 		this.adapter = new AssignmentsAdapter( this.getActivity() );
 		this.setListAdapter( adapter );
 
@@ -58,15 +56,31 @@ public class AssignmentsFragment extends ListFragment implements
 		super.onCreateView( inflater, container, savedInstanceState );
 		View view = inflater.inflate( R.layout.fragment_assignments, container,
 				false );
+
+		emptyView = (TextView) view
+				.findViewById( R.id.fragment_assignments_empty );
+		( (ListView) view.findViewById( android.R.id.list ) )
+				.setEmptyView( emptyView );
 		return view;
+	}
+
+	@Override
+	public void onViewCreated( View view, Bundle savedInstanceState )
+	{
+		this.getActivity().getSupportLoaderManager()
+				.initLoader( LoaderCodes.LOADER_CODE_ASSIGNMENTS, null, this )
+				.forceLoad();
+		super.onViewCreated( view, savedInstanceState );
 	}
 
 	@Override
 	public void setEmptyText( CharSequence text )
 	{
-		/* Subclasses which use custom views must override this method. */
-		( (TextView) this.getActivity().findViewById( android.R.id.empty ) )
-				.setText( text );
+		/*
+		 * Subclasses using custom layouts must override this method to operate
+		 * on the correct TextView.
+		 */
+		emptyView.setText( text );
 	}
 
 	@Override
@@ -76,6 +90,8 @@ public class AssignmentsFragment extends ListFragment implements
 		{
 			case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
 			{
+				this.setEmptyText( this
+						.getString( R.string.fragment_assignments_list_loading_message ) );
 				return new AssignmentsLoader( this.getActivity(),
 						assigmentsDataSource, this.getCourse().getId() );
 			}
@@ -112,8 +128,6 @@ public class AssignmentsFragment extends ListFragment implements
 			case LoaderCodes.LOADER_CODE_ASSIGNMENTS:
 			{
 				this.adapter.clear();
-				this.setEmptyText( this
-						.getString( R.string.fragment_assignments_list_loading_message ) );
 				break;
 			}
 		}
