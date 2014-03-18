7f2fedee46d974f43b720832d8a49e1f97970c71
Alex Morgan <axemorgan@gmail.com>
2014-03-17 20:13:57 -0400

    Created data model class for grades.

---
+++ src/org/cascadelms/data/models/Grade.java
@@ -0,0 +1,78 @@
+package org.cascadelms.data.models;
+
+import java.util.Date;
+
+import org.cascadelms.data.models.Assignment.Category;
+
+/**
+ * An immutable model class representing a grade from Cascade LMS database.
+ * <p>
+ * This class does not use a builder because all fields are mandatory.
+ */
+public class Grade
+{
+	private final int assignmentId;
+	private final String assignmentTitle;
+	private final Category assignmentCategory;
+	private final Date postDate;
+	private final Type gradeType;
+	private final double pointsEarned;
+	private final double pointsPossible;
+
+	public Grade( int assignmentId, String assignmentTitle,
+			Category assignmentCategory, Date postDate, Type gradeType,
+			double pointsEarned, double pointsPossible )
+	{
+		this.assignmentId = assignmentId;
+		this.assignmentTitle = assignmentTitle;
+		this.assignmentCategory = assignmentCategory;
+		this.postDate = postDate;
+		this.gradeType = gradeType;
+		this.pointsEarned = pointsEarned;
+		this.pointsPossible = pointsPossible;
+	}
+
+	public int getAssignmentId()
+	{
+		return assignmentId;
+	}
+
+	public String getAssignmentTitle()
+	{
+		return assignmentTitle;
+	}
+
+	public Category getAssignmentCategory()
+	{
+		return assignmentCategory;
+	}
+
+	public Date getPostDate()
+	{
+		return postDate;
+	}
+
+	public Type getGradeType()
+	{
+		return gradeType;
+	}
+
+	public double getPointsEarned()
+	{
+		return pointsEarned;
+	}
+
+	public double getPointsPossible()
+	{
+		return pointsPossible;
+	}
+
+	/**
+	 * Possible values for a Grade's grade_type attribute. So far, only Score
+	 * has been observed.
+	 */
+	public enum Type
+	{
+		SCORE
+	}
+}
