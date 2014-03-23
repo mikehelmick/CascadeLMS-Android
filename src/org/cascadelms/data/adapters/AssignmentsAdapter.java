ccb0ca54c81d05b61c49e6f2e0a581f894dcd4fb
Alex Morgan <axemorgan@gmail.com>
2014-03-23 12:19:02 -0400

    Added categories to the Assignments list.

--- src/org/cascadelms/data/adapters/AssignmentsAdapter.java
+++ src/org/cascadelms/data/adapters/AssignmentsAdapter.java
@@ -3,10 +3,12 @@ package org.cascadelms.data.adapters;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.cascadelms.R;
 import org.cascadelms.data.models.Assignment;
+import org.cascadelms.data.models.Assignment.AssignmentComparator;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -17,25 +19,54 @@ import android.widget.TextView;
 
 public class AssignmentsAdapter extends BaseAdapter
 {
-	Context context;
-	ArrayList<Assignment> assignments;
+	private Context context;
+	private ArrayList<Object> items;
+
+	/* Constants */
+	private static final int ITEM_TYPE_ASSIGNMENT = 0;
+	private static final int ITEM_TYPE_DIVIDER = 1;
 
 	public AssignmentsAdapter( Context context )
 	{
 		this.context = context;
-		this.assignments = new ArrayList<Assignment>();
+		this.items = new ArrayList<Object>();
+	}
+
+	@Override
+	public boolean areAllItemsEnabled()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled( int position )
+	{
+		{
+			Object item = items.get( position );
+			if( item instanceof Assignment )
+			{
+				return true;
+			} else if( item instanceof Divider )
+			{
+				return false;
+			} else
+			{
+				throw new IllegalStateException(
+						"An item in the list was neither an Assignment nor Divider." );
+			}
+		}
 	}
 
 	@Override
 	public int getCount()
 	{
-		return assignments.size();
+		return items.size();
 	}
 
 	@Override
 	public Object getItem( int position )
 	{
-		return assignments.get( position );
+		return items.get( position );
 	}
 
 	@Override
@@ -45,85 +76,191 @@ public class AssignmentsAdapter extends BaseAdapter
 	}
 
 	@Override
-	public View getView( int position, View convertView, ViewGroup parent )
+	public int getViewTypeCount()
 	{
-		/* Re-uses the convertView, if possible. */
-		View view;
-		if( convertView == null )
-		{
-			view = LayoutInflater.from( this.context ).inflate(
-					R.layout.list_item_assignment, parent, false );
-		} else
-		{
-			view = convertView;
-		}
-		Assignment assignment = (Assignment) this.getItem( position );
+		return 2;
+	}
 
-		/* Sets the list item text according to the Document's title. */
-		( (TextView) view.findViewById( R.id.list_item_assignment_title ) )
-				.setText( assignment.toString() );
-
-		/* Sets the points earned/possible text. */
-		String pointsString;
-		NumberFormat format = DecimalFormat.getNumberInstance();
-		format.setMaximumFractionDigits( 2 );
-		if( Double.compare( assignment.getPointsEarned(), Double.NaN ) == 0 )
+	@Override
+	public int getItemViewType( int position )
+	{
+		Object item = items.get( position );
+		if( item instanceof Assignment )
 		{
-			pointsString = this.context.getString(
-					R.string.list_item_assignment_no_points,
-					format.format( assignment.getPointsPossible() ) );
-		} else
+			return ITEM_TYPE_ASSIGNMENT;
+		} else if( item instanceof Divider )
 		{
-			pointsString = this.context.getString(
-					R.string.list_item_assignment_points,
-					format.format( assignment.getPointsEarned() ),
-					format.format( assignment.getPointsPossible() ) );
-		}
-		( (TextView) view.findViewById( R.id.list_item_assignment_points ) )
-				.setText( pointsString );
-
-		/* Sets the date field to the next upcoming date of the assignment. */
-		String dateString = "";
-		if( assignment.isUpcoming() )
-		{
-			dateString = this.context.getString(
-					R.string.list_item_assignment_upcoming_date,
-					assignment.getOpenDate() );
-		} else if( assignment.isCurrent() )
-		{
-			dateString = this.context.getString(
-					R.string.list_item_assignment_due_date,
-					assignment.getDueDate() );
-		} else if( assignment.isPastDue() )
-		{
-			dateString = this.context.getString(
-					R.string.list_item_assignment_close_date,
-					assignment.getCloseDate() );
-		} else if( assignment.isClosed() )
-		{
-			dateString = this.context
-					.getString( R.string.list_item_assignment_closed );
+			return ITEM_TYPE_DIVIDER;
 		} else
 		{
 			throw new IllegalStateException(
-					"The assignment must be one of upcoming/current/past/closed" );
+					"An item in the list was neither an Assignment nor Divider." );
 		}
-		( (TextView) view.findViewById( R.id.list_item_assignment_date ) )
-				.setText( dateString );
-
-		return view;
 	}
 
-	public void setData( List<Assignment> data )
+	@Override
+	public View getView( int position, View convertView, ViewGroup parent )
 	{
-		this.assignments.clear();
-		this.assignments.addAll( data );
+		switch( this.getItemViewType( position ) )
+		{
+			case ITEM_TYPE_ASSIGNMENT:
+			{
+				/* Re-uses the convertView, if possible. */
+				View view;
+				if( convertView == null )
+				{
+					view = LayoutInflater.from( this.context ).inflate(
+							R.layout.list_item_assignment, parent, false );
+				} else
+				{
+					view = convertView;
+				}
+				Assignment assignment = (Assignment) this.getItem( position );
+
+				/* Sets the list item text according to the Document's title. */
+				( (TextView) view
+						.findViewById( R.id.list_item_assignment_title ) )
+						.setText( assignment.toString() );
+
+				/* Sets the points earned/possible text. */
+				String pointsString;
+				NumberFormat format = DecimalFormat.getNumberInstance();
+				format.setMaximumFractionDigits( 2 );
+				if( Double.compare( assignment.getPointsEarned(), Double.NaN ) == 0 )
+				{
+					pointsString = this.context.getString(
+							R.string.list_item_assignment_no_points,
+							format.format( assignment.getPointsPossible() ) );
+				} else
+				{
+					pointsString = this.context.getString(
+							R.string.list_item_assignment_points,
+							format.format( assignment.getPointsEarned() ),
+							format.format( assignment.getPointsPossible() ) );
+				}
+				( (TextView) view
+						.findViewById( R.id.list_item_assignment_points ) )
+						.setText( pointsString );
+
+				/*
+				 * Sets the date field to the next upcoming date of the
+				 * assignment.
+				 */
+				String dateString = "";
+				if( assignment.isUpcoming() )
+				{
+					dateString = this.context.getString(
+							R.string.list_item_assignment_upcoming_date,
+							assignment.getOpenDate() );
+				} else if( assignment.isCurrent() )
+				{
+					dateString = this.context.getString(
+							R.string.list_item_assignment_due_date,
+							assignment.getDueDate() );
+				} else if( assignment.isPastDue() )
+				{
+					dateString = this.context.getString(
+							R.string.list_item_assignment_close_date,
+							assignment.getCloseDate() );
+				} else if( assignment.isClosed() )
+				{
+					dateString = this.context
+							.getString( R.string.list_item_assignment_closed );
+				} else
+				{
+					throw new IllegalStateException(
+							"The assignment must be one of upcoming/current/past/closed" );
+				}
+				( (TextView) view.findViewById( R.id.list_item_assignment_date ) )
+						.setText( dateString );
+
+				return view;
+			}
+			case ITEM_TYPE_DIVIDER:
+			{
+				/* TODO create an actual XML file to inflate for dividers. */
+				TextView dividerView = (TextView) LayoutInflater.from(
+						this.context ).inflate( R.layout.list_item_document,
+						parent, false );
+				dividerView
+						.setText( ( (Divider) this.getItem( position ) ).title );
+				return dividerView;
+			}
+			default:
+			{
+				throw new IllegalStateException(
+						"Unrecognized item type in AssignmentsAdapter.getView()" );
+			}
+		}
+
+	}
+
+	public void setData( List<? extends Assignment> data )
+	{
+		/* Sorts the Documents and replaces the list with them. */
+		Collections.sort( data, new AssignmentComparator() );
+		this.items.clear();
+		this.items.addAll( data );
+		if( data.isEmpty() )
+		{
+			/* Nothing left to do. */
+			return;
+		}
+
+		/* Adds in list separators at the appropriate indexes */
+		int index = 0;
+		if( ( (Assignment) items.get( 0 ) ).isUpcoming() )
+		{
+			items.add( 0, new Divider( "Upcoming" ) ); // TODO Use string
+														// resource.
+			index = 1;
+		}
+		for ( int i = index; i < items.size(); i++ )
+		{
+			if( ( (Assignment) items.get( i ) ).isCurrent() )
+			{
+				items.add( i, new Divider( "Current" ) ); // TODO Use string
+															// resource.
+				index = i + 1;
+				break;
+			}
+		}
+		for ( int i = index; i < items.size(); i++ )
+		{
+			if( ( (Assignment) items.get( i ) ).isPastDue() )
+			{
+				items.add( i, new Divider( "Past Due" ) ); // TODO Use string
+															// resource.
+				index = i + 1;
+				break;
+			}
+		}
+		for ( int i = index; i < items.size(); i++ )
+		{
+			if( ( (Assignment) items.get( i ) ).isClosed() )
+			{
+				items.add( i, new Divider( "Closed" ) ); // TODO Use string
+															// resource.
+				break;
+			}
+		}
+
 		this.notifyDataSetChanged();
 	}
 
 	public void clear()
 	{
-		this.assignments.clear();
+		this.items.clear();
 		this.notifyDataSetInvalidated();
+	}
+
+	private class Divider
+	{
+		private String title;
+
+		private Divider( String title )
+		{
+			this.title = title;
+		}
 	}
 }
