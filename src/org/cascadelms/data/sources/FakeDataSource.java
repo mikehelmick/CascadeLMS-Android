ab65c00e11df9320ff90351dac10b9ae7949ebf5
Alex Morgan <axemorgan@gmail.com>
2014-03-31 12:19:49 -0400

    Re-added pretty data for the poster screenshot.

--- src/org/cascadelms/data/sources/FakeDataSource.java
+++ src/org/cascadelms/data/sources/FakeDataSource.java
@@ -261,6 +261,28 @@ public class FakeDataSource implements SchoolsDataSource, CourseDataSource,
 		streamItemsListInfo = new ArrayList<StreamItem>();
 		streamItemsListUI = new ArrayList<StreamItem>();
 
+		streamItemsListAll
+				.add( new StreamItem(
+						1000,
+						StreamItem.ItemType.DOCUMENT,
+						new Date(),
+						"A new document was posted in CS5002: Senior Design: Post Design Guidelines",
+						null ) );
+		streamItemsListAll
+				.add( new StreamItem(
+						2000,
+						StreamItem.ItemType.BLOG_POST,
+						new Date( System.currentTimeMillis() - 86400 ),
+						"All FINAL abstracts (150 word count), titles and equipment needs are to be submitted no later than Wednesday February 26, 2014.  The links below are to be used for your project abstract submissions and for equipment needs – one each per project. ",
+						null ) );
+		streamItemsListAll
+				.add( new StreamItem(
+						3000,
+						StreamItem.ItemType.ASSIGNMENT,
+						new Date( System.currentTimeMillis() - 2 * 86400 ),
+						"A new assignment was poster in CS5002: Senior Design: Final Expo Poster",
+						null ) );
+
 		for ( Assignment assignment : assignmentsListDesign )
 		{
 			StreamItem item = new StreamItem( assignment.getId(),
