403dab8ebdbc9638ed0562283fe7286e715f68a2
Cosmic Chip Socket <hagersdk@mail.uc.edu>
2014-04-04 17:09:33 -0400

    Return of the Social Stream detail view

---
+++ src/org/cascadelms/StreamDetailActivity.java
@@ -0,0 +1,107 @@
+package org.cascadelms;
+
+import org.cascadelms.data.adapters.StreamItemDetailAdapter;
+import org.cascadelms.data.loaders.StreamItemDetailLoader;
+import org.cascadelms.data.loaders.LoaderCodes;
+import org.cascadelms.data.models.StreamItem;
+import org.cascadelms.data.sources.FakeDataSource;
+import org.cascadelms.fragments.SocialStreamFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class StreamDetailActivity extends CascadeActivity implements
+        LoaderCallbacks<StreamItem>
+{
+    private long postId;
+    private SocialStreamFragment.StreamDataSource streamDataSource;
+    private StreamItemDetailAdapter adapter;
+
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView(R.layout.activity_stream_detail);
+
+        /* Gets the Course data provided by the Intent that started this. */
+        Bundle extras = this.getIntent().getExtras();
+        this.postId = extras.getLong(SocialStreamFragment.ARGS_POSTID);
+
+		/* Initializes a data source and begins loading. */
+        this.streamDataSource = FakeDataSource.getInstance();
+        this.adapter = new StreamItemDetailAdapter(this);
+
+        this.getSupportLoaderManager()
+                .initLoader(LoaderCodes.LOADER_CODE_STREAM_DETAIL, null, this).forceLoad();
+
+        ListView streamList = (ListView) findViewById( android.R.id.list );
+
+        if (streamList != null)
+        {
+            streamList.setAdapter(adapter);
+        }
+
+        // No parent in the XML, display as up programmatically.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<StreamItem> onCreateLoader( int id, Bundle args )
+    {
+        switch( id )
+        {
+            case LoaderCodes.LOADER_CODE_STREAM_DETAIL:
+            {
+                return new StreamItemDetailLoader( this,
+                        streamDataSource, postId );
+            }
+            default:
+            {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public void onLoadFinished( Loader<StreamItem> loader,
+                                StreamItem data )
+    {
+        switch( loader.getId() )
+        {
+            case LoaderCodes.LOADER_CODE_STREAM_DETAIL:
+            {
+                adapter.setItem(data);
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset( Loader<StreamItem> loader )
+    {
+        switch( loader.getId() )
+        {
+            case LoaderCodes.LOADER_CODE_STREAM_DETAIL:
+            {
+                adapter.setItem(null);
+                break;
+            }
+        }
+    }
+}
---
+++ src/org/cascadelms/data/adapters/StreamItemDetailAdapter.java
@@ -0,0 +1,135 @@
+package org.cascadelms.data.adapters;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.cascadelms.R;
+import org.cascadelms.data.models.Comment;
+import org.cascadelms.data.models.StreamItem;
+
+import java.text.SimpleDateFormat;
+
+public class StreamItemDetailAdapter extends BaseAdapter
+{
+    Context context;
+    StreamItem item;
+
+    public StreamItemDetailAdapter(Context context)
+    {
+        this.context = context;
+    }
+
+    public void setItem(StreamItem item)
+    {
+        this.item = item;
+    }
+
+    @Override
+    public int getCount()
+    {
+        if (item == null)
+            return 0;
+        else if (item.getComments() == null)
+            return 1;
+        return 1 + item.getComments().length;
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        if (position == 0)
+            return item;
+        else if (item != null && item.getComments() != null)
+            return item.getComments()[position - 1];
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+    @Override
+    public View getView( int position, View convertView, ViewGroup parent )
+    {
+        if (item == null)
+            return null;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, K:m a");
+
+        if (position == 0)
+        {
+            /* Inflates a new view if the adapter doesn't provide one to reuse. */
+            //if (convertView == null)
+            {
+                convertView = LayoutInflater.from(context).inflate(
+                        R.layout.list_item_socialstream_post, parent, false);
+            }
+
+            if (convertView != null)
+            {
+                TextView dateLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_date_course);
+                dateLabel.setText(dateFormat.format(item.getSummaryDate()));
+                TextView authorLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_author);
+                authorLabel.setText(item.getAuthor());
+                TextView summaryLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_description);
+                summaryLabel.setText(item.getSummary());
+                TextView scoreLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_aplus_count);
+                scoreLabel.setText(Integer.toString(item.getScore()));
+                TextView commentLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_comment_count);
+
+                int commentCount = item.getCommentCount();
+
+                commentLabel.setText(convertView.getResources().getQuantityString(R.plurals.comments,
+                        commentCount, commentCount));
+
+                Drawable authorAvatar = item.getAuthorAvatar();
+
+                if (authorAvatar != null)
+                {
+                    ImageView avatarImage = (ImageView) convertView
+                            .findViewById(R.id.socialstream_avatar);
+                    avatarImage.setImageDrawable(authorAvatar);
+                }
+            }
+        }
+        else
+        {
+            // TODO: Reenable convertView
+            //if (convertView == null)
+            {
+                convertView = LayoutInflater.from(context).inflate(
+                        R.layout.list_item_socialstream_comment, parent, false);
+            }
+
+            if (convertView != null)
+            {
+                Comment comment = item.getComments()[position - 1];
+
+                TextView dateLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_date);
+                dateLabel.setText(dateFormat.format(comment.getCreatedAt()));
+                TextView authorLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_author);
+                authorLabel.setText(comment.getAuthor());
+                TextView summaryLabel = (TextView) convertView
+                        .findViewById(R.id.socialstream_description);
+                summaryLabel.setText(comment.getBody());
+            }
+        }
+
+        return convertView;
+    }
+}
