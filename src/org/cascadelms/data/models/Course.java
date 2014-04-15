9ae9e2bb5b6a1657eec515be32f988ea54b72655
Alex Morgan <axemorgan@gmail.com>
2014-04-14 20:51:59 -0400

    Finished parseCourseList implementation.

--- src/org/cascadelms/data/models/Course.java
+++ src/org/cascadelms/data/models/Course.java
@@ -146,4 +146,57 @@ public class Course implements Parcelable
 			return new Course[size];
 		}
 	};
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ( current ? 1231 : 1237 );
+		result = prime * result
+				+ ( ( description == null ) ? 0 : description.hashCode() );
+		result = prime * result + id;
+		result = prime * result
+				+ ( ( semester == null ) ? 0 : semester.hashCode() );
+		result = prime * result + termId;
+		result = prime * result + ( ( title == null ) ? 0 : title.hashCode() );
+		return result;
+	}
+
+	@Override
+	public boolean equals( Object obj )
+	{
+		if( this == obj )
+			return true;
+		if( obj == null )
+			return false;
+		if( getClass() != obj.getClass() )
+			return false;
+		Course other = (Course) obj;
+		if( current != other.current )
+			return false;
+		if( description == null )
+		{
+			if( other.description != null )
+				return false;
+		} else if( !description.equals( other.description ) )
+			return false;
+		if( id != other.id )
+			return false;
+		if( semester == null )
+		{
+			if( other.semester != null )
+				return false;
+		} else if( !semester.equals( other.semester ) )
+			return false;
+		if( termId != other.termId )
+			return false;
+		if( title == null )
+		{
+			if( other.title != null )
+				return false;
+		} else if( !title.equals( other.title ) )
+			return false;
+		return true;
+	}
 }
